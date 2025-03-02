f2693e443fbb4b5ca4c3943489e229f0546e6768
Andreas Nilsson <anil9@kth.se>
2025-03-02 12:54:34 +0100

    View clips without logging in to twitch

--- src/main/java/com/nilsson/sentiment/endpoint/api/TwitchChatStreamingController.java
+++ src/main/java/com/nilsson/sentiment/endpoint/api/TwitchChatStreamingController.java
@@ -7,12 +7,14 @@ import lombok.NonNull;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/secured/api")
 public class TwitchChatStreamingController {
     @NonNull
     private final TwitchChatService twitchChatService;
